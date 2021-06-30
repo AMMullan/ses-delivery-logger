c766e8960090a6ea22c08ca721e6a4c9db7c5170
Allan Mullan <allan.mullan@informa.com>
2021-06-30 09:43:53 +0100

    Fixed IAM Role Def

--- iam.tf
+++ iam.tf
@@ -1,15 +1,19 @@
 resource "aws_iam_role" "notification_handler" {
-  name = "${var.iam_role_prefix}-${var.region}"
+  name = "${var.iam_role_prefix}-${data.aws_region.current.name}"
 
-  assume_role_policy = file("resources/iam/notification_handler_trust.json")
+  assume_role_policy = file("${path.module}/resources/iam/notification_handler_trust.json")
 
-  inline_policy = templatefile(
-    "resources/iam/notification_handler_policy.json",
-    {
-      account_id                 = var.account_id,
-      region                     = var.region,
-      dynamodb_table             = var.ddb_tbl_name,
-      cloudwatch_logs_group_name = var.cloudwatch_logs_group_name
-    }
-  )
+  inline_policy {
+    name = "SESNotificationHandlerPermissions"
+
+    policy = templatefile(
+      "${path.module}/resources/iam/notification_handler_policy.json",
+      {
+        account_id                 = data.aws_caller_identity.current.account_id,
+        region                     = data.aws_region.current.name,
+        dynamodb_table             = var.ddb_tbl_name,
+        cloudwatch_logs_group_name = var.cloudwatch_logs_group_name
+      }
+    )
+  }
 }
