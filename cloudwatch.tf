4e8708e25ad88851c0567321b4ae1b6d993aaea4
Allan Mullan <allan.mullan@informa.com>
2021-07-01 16:27:57 +0100

    Remove region and disable KMS

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,8 +1,7 @@
+#tfsec:ignore:AWS089
 resource "aws_cloudwatch_log_group" "notification_handler_lambda" {
-  name              = "${var.cloudwatch_logs_group_name}-${data.aws_region.current.name}"
+  name              = var.cloudwatch_logs_group_name
   retention_in_days = var.cloudwatch_logs_retention
-
-  kms_key_id = aws_kms_key.notification_handler.arn
 
   tags = var.resource_tags
 }
