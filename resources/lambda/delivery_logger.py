c6ec5f489c09b686a6434435286c8cd9dfdbdc1f
Allan Mullan <ammullan@gmail.com>
2022-02-14 10:39:02 +0000

    Made destinations a SS

--- resources/lambda/delivery_logger.py
+++ resources/lambda/delivery_logger.py
@@ -42,11 +42,11 @@ def lambda_handler(event, context):
     ddb_item["EventType"] = {'S': event_type}
 
     # Record the subject if the Headers are included
-    eml_subject = message.get('mail').get('commonHeaders').get('subject')
+    eml_subject = message.get('mail', {}).get('commonHeaders', {}).get('subject')
     if eml_subject:
         ddb_item['Subject'] = {'S': eml_subject}
 
-    destination_address = str(message.get('mail').get('destination'))
+    destination_address = message.get('mail').get('destination')
 
     config_set = message.get('mail').get('tags', {}).get('ses:configuration-set')
     if config_set:
@@ -59,7 +59,7 @@ def lambda_handler(event, context):
         bounce_detail = message.get('bounce')
 
         ddb_item['BounceSummary'] = {'S': json.dumps(bounce_detail.get('bouncedRecipients'))}
-        ddb_item['DestinationAddress'] = {'S': destination_address}
+        ddb_item['DestinationAddress'] = {'SS': destination_address}
         ddb_item['ReportingMTA'] = {'S': bounce_detail.get('reportingMTA', '')}
         ddb_item['BounceType'] = {'S': bounce_detail.get('bounceType')}
         ddb_item['BounceSubType'] = {'S': bounce_detail.get('bounceSubType')}
@@ -69,7 +69,7 @@ def lambda_handler(event, context):
         complaint_detail = message.get('complaint')
 
         ddb_item['ComplaintSummary'] = {'S': json.dumps(complaint_detail.get('complainedRecipients'))}
-        ddb_item['DestinationAddress'] = {'S': destination_address}
+        ddb_item['DestinationAddress'] = {'SS': destination_address}
         ddb_item['FeedbackId'] = {'S': complaint_detail.get('feedbackId')}
         ddb_item['FeedbackType'] = {'S': complaint_detail.get('complaintFeedbackType')}
         ddb_item['MessageTime'] = {'S': complaint_detail.get('arrivalDate')}
@@ -77,7 +77,7 @@ def lambda_handler(event, context):
     elif event_type == 'Delivery':
         delivery_detail = message.get('delivery')
 
-        ddb_item['DestinationAddress'] = {'S': str(delivery_detail.get('recipients'))}
+        ddb_item['DestinationAddress'] = {'SS': delivery_detail.get('recipients')}
         ddb_item['ReportingMTA'] = {'S': delivery_detail.get('reportingMTA', '')}
         ddb_item['SMTPResponse'] = {'S': delivery_detail.get('smtpResponse')}
         ddb_item['MessageTime'] = {'S': delivery_detail.get('timestamp')}
@@ -91,20 +91,23 @@ def lambda_handler(event, context):
         ddb_item['MessageTime'] = {'S': delay_detail.get('timestamp')}
 
     elif event_type == 'Reject':
-        ddb_item['Reason'] = {'S': message.get('reject').reason()}
+        ddb_item['DestinationAddress'] = {'SS': destination_address}
+        ddb_item['Reason'] = {'S': message.get('reject').get('reason')}
 
     elif event_type == 'Click':
         click_detail = message.get('click')
 
+        ddb_item['DestinationAddress'] = {'SS': destination_address}
         ddb_item['IPAddress'] = {'S': click_detail.get('ipAddress')}
         ddb_item['Link'] = {'S': click_detail.get('link')}
-        ddb_item['LinkTags'] = {'S': click_detail.get('linkTags')}
+        ddb_item['LinkTags'] = {'S': json.dumps(click_detail.get('linkTags'))}
         ddb_item['UserAgent'] = {'S': click_detail.get('userAgent')}
         ddb_item['MessageTime'] = {'S': click_detail.get('timestamp')}
 
     elif event_type == 'Open':
         open_detail = message.get('open')
 
+        ddb_item['DestinationAddress'] = {'SS': destination_address}
         ddb_item['IPAddress'] = {'S': open_detail.get('ipAddress')}
         ddb_item['UserAgent'] = {'S': open_detail.get('userAgent')}
         ddb_item['MessageTime'] = {'S': open_detail.get('timestamp')}
