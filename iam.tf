4ff6c0f2a944a9c3f0e0b5f036635282eba6c1b3
Allan Mullan <ammullan@gmail.com>
2022-02-18 13:53:18 +0000

    Add CloudWatch Logs destination to IAM Policy

--- iam.tf
+++ iam.tf
@@ -9,10 +9,11 @@ resource "aws_iam_role" "delivery_logger" {
     policy = templatefile(
       "${path.module}/resources/iam/delivery_logger_policy.json",
       {
-        account_id                 = data.aws_caller_identity.current.account_id,
-        region                     = data.aws_region.current.name,
-        dynamodb_table             = var.ddb_tbl_name,
-        cloudwatch_logs_group_name = "/aws/lambda/${var.lambda_name}"
+        account_id                  = data.aws_caller_identity.current.account_id,
+        region                      = data.aws_region.current.name,
+        dynamodb_table              = var.ddb_tbl_name,
+        cloudwatch_logs_group_name  = "/aws/lambda/${var.lambda_name}",
+        cloudwatch_logs_destination = var.cloudwatch_logs_destination
       }
     )
   }
